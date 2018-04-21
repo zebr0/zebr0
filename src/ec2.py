82f36e822ee82f8ec4de270a98d05703138b25ed
mazerty <mazerty@gmail.com>
2018-04-21 16:10:52 +0200

    rewrote a little : ec2 module

--- src/ec2.py
+++ src/ec2.py
@@ -1,5 +1,13 @@
+import boto3
+
 describe_images_requests_map = {
     "ubuntu-xenial": {"Filters": [{"Name": "name",
                                    "Values": ["ubuntu/images/hvm-ssd/ubuntu-xenial-*"]}],
                       "Owners": ["099720109477"]}
 }
+
+ec2_client = boto3.client("ec2")
+
+
+def latest_image(distribution):
+    return ec2_client.describe_images(**describe_images_requests_map[distribution])
