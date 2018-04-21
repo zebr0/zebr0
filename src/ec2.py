820e75613498924c7746488b4a64f061f4dceb83
mazerty <mazerty@gmail.com>
2018-04-21 16:33:57 +0200

    fixed latest image

--- src/ec2.py
+++ src/ec2.py
@@ -1,6 +1,6 @@
 import boto3
 
-describe_images_requests_map = {
+describe_images_requests = {
     "ubuntu-xenial": {"Filters": [{"Name": "name",
                                    "Values": ["ubuntu/images/hvm-ssd/ubuntu-xenial-*"]}],
                       "Owners": ["099720109477"]}
@@ -10,4 +10,7 @@ ec2_client = boto3.client("ec2")
 
 
 def latest_image(distribution):
-    return ec2_client.describe_images(**describe_images_requests_map[distribution])
+    response = ec2_client.describe_images(**describe_images_requests[distribution])
+    images = response["Images"]
+    images.sort(key=lambda image: image["CreationDate"], reverse=True)
+    return images[0]
