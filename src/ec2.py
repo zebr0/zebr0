3327d470db29c245fd23bb4aded09b6fdd884f61
mazerty <mazerty@gmail.com>
2018-05-21 11:14:44 +0200

    factor tags creation

--- src/ec2.py
+++ src/ec2.py
@@ -7,23 +7,6 @@ import config
 client = boto3.client("ec2")
 
 
-def fetch_latest_image_id(distribution: str) -> str:
-    request = {
-        "ubuntu-xenial": {"Filters": [{"Name": "name",
-                                       "Values": ["ubuntu/images/hvm-ssd/ubuntu-xenial-*"]}],
-                          "Owners": ["099720109477"]},
-        "ubuntu-bionic": {"Filters": [{"Name": "name",
-                                       "Values": ["ubuntu/images/hvm-ssd/ubuntu-bionic-*"]}],
-                          "Owners": ["099720109477"]}
-    }
-
-    response = client.describe_images(**request.get(distribution))
-    images = response.get("Images")
-    images.sort(key=lambda image: image.get("CreationDate"), reverse=True)
-    latest = images[0]
-    return latest.get("ImageId")
-
-
 def init_environment(project: str, stage: str):
     create_vpc_if_needed(project, stage)
     create_instance_if_needed(project, stage)
@@ -42,13 +25,7 @@ def create_vpc_if_needed(project, stage):
         response = client.create_vpc(**request)
         pprint.pprint(response)
 
-        request = {
-            "Resources": [response.get("Vpc").get("VpcId")],
-            "Tags": [{"Key": "project", "Value": project},
-                     {"Key": "stage", "Value": stage}]
-        }
-        response = client.create_tags(**request)
-        pprint.pprint(response)
+        create_tags(project, stage, response.get("Vpc").get("VpcId"))
 
 
 def create_instance_if_needed(project, stage):
@@ -69,10 +46,31 @@ def create_instance_if_needed(project, stage):
         response = client.run_instances(**request)
         pprint.pprint(response)
 
-        request = {
-            "Resources": [response.get("Instances")[0].get("InstanceId")],
-            "Tags": [{"Key": "project", "Value": project},
-                     {"Key": "stage", "Value": stage}]
-        }
-        response = client.create_tags(**request)
-        pprint.pprint(response)
+        create_tags(project, stage, response.get("Instances")[0].get("InstanceId"))
+
+
+def create_tags(project, stage, resource_id):
+    request = {
+        "Resources": [resource_id],
+        "Tags": [{"Key": "project", "Value": project},
+                 {"Key": "stage", "Value": stage}]
+    }
+    response = client.create_tags(**request)
+    pprint.pprint(response)
+
+
+def fetch_latest_image_id(distribution: str) -> str:
+    request = {
+        "ubuntu-xenial": {"Filters": [{"Name": "name",
+                                       "Values": ["ubuntu/images/hvm-ssd/ubuntu-xenial-*"]}],
+                          "Owners": ["099720109477"]},
+        "ubuntu-bionic": {"Filters": [{"Name": "name",
+                                       "Values": ["ubuntu/images/hvm-ssd/ubuntu-bionic-*"]}],
+                          "Owners": ["099720109477"]}
+    }
+
+    response = client.describe_images(**request.get(distribution))
+    images = response.get("Images")
+    images.sort(key=lambda image: image.get("CreationDate"), reverse=True)
+    latest = images[0]
+    return latest.get("ImageId")
