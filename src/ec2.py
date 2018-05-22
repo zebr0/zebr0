906e492b10df651722d687e192865c27b6ea7d01
mazerty <mazerty@gmail.com>
2018-05-22 21:15:02 +0200

    refactor: back to the usual way of calling boto3 functions

--- src/ec2.py
+++ src/ec2.py
@@ -1,5 +1,3 @@
-import pprint
-
 import boto3
 
 import config
@@ -14,62 +12,57 @@ def init_environment(project: str, stage: str):
 
 def create_vpc_if_needed(project: str, stage: str) -> str:
     print("checking vpc")
-    request = {"Filters": [{"Name": "tag:project", "Values": [project]},
-                           {"Name": "tag:stage", "Values": [stage]}]}
-    response = client.describe_vpcs(**request)
+    describe_vpcs = client.describe_vpcs(
+        Filters=[{"Name": "tag:project", "Values": [project]},
+                 {"Name": "tag:stage", "Values": [stage]}]
+    )
 
-    if not response.get("Vpcs"):
+    if not describe_vpcs.get("Vpcs"):
         print("vpc not found, creating vpc")
         network_cidr = config.fetch_network_cidr(project, stage)
-        request = {
-            "CidrBlock": network_cidr
-        }
-        response = client.create_vpc(**request)
-        vpc_id = response.get("Vpc").get("VpcId")
+        create_vpc = client.create_vpc(CidrBlock=network_cidr)
+        vpc_id = create_vpc.get("Vpc").get("VpcId")
         client.get_waiter('vpc_exists').wait(VpcIds=[vpc_id])
         create_tags(project, stage, vpc_id)
 
         print("creating subnet")
-        request = {
-            "CidrBlock": network_cidr,
-            "VpcId": vpc_id
-        }
-        response = client.create_subnet(**request)
-        subnet_id = response.get("Subnet").get("SubnetId")
+        create_subnet = client.create_subnet(CidrBlock=network_cidr, VpcId=vpc_id)
+        subnet_id = create_subnet.get("Subnet").get("SubnetId")
         create_tags(project, stage, subnet_id)
 
         return subnet_id
 
 
-def create_instance_if_needed(project, stage):
-    request = {"Filters": [{"Name": "tag:project", "Values": [project]},
-                           {"Name": "tag:stage", "Values": [stage]},
-                           {"Name": "instance-state-name", "Values": ["pending", "running", "stopping", "stopped"]}]}
-    response = client.describe_instances(**request)
-    pprint.pprint(response)
+def create_instance_if_needed(project: str, stage: str) -> str:
+    print("checking instance")
+    describe_instances = client.describe_instances(
+        Filters=[{"Name": "tag:project", "Values": [project]},
+                 {"Name": "tag:stage", "Values": [stage]},
+                 {"Name": "instance-state-name", "Values": ["pending", "running", "stopping", "stopped"]}]
+    )
 
-    if not response.get("Reservations"):
-        request = {
-            "ImageId": fetch_latest_image_id(config.fetch_distribution(project)),
-            "MinCount": 1,
-            "MaxCount": 1,
-            "KeyName": "keypair",
-            "InstanceType": config.fetch_instance_type(project, stage)
-        }
-        response = client.run_instances(**request)
-        pprint.pprint(response)
+    if not describe_instances.get("Reservations"):
+        print("instance not found, creating instance")
+        run_instances = client.run_instances(
+            ImageId=fetch_latest_image_id(config.fetch_distribution(project)),
+            MinCount=1,
+            MaxCount=1,
+            KeyName="keypair",
+            InstanceType=config.fetch_instance_type(project, stage)
+        )
+        instance_id = run_instances.get("Instances")[0].get("InstanceId")
+        create_tags(project, stage, instance_id)
 
-        create_tags(project, stage, response.get("Instances")[0].get("InstanceId"))
+        return instance_id
 
 
-def create_tags(project, stage, resource_id):
-    request = {
-        "Resources": [resource_id],
-        "Tags": [{"Key": "project", "Value": project},
-                 {"Key": "stage", "Value": stage}]
-    }
-    response = client.create_tags(**request)
-    pprint.pprint(response)
+def create_tags(project: str, stage: str, resource_id: str):
+    print("updating tags")
+    client.create_tags(
+        Resources=[resource_id],
+        Tags=[{"Key": "project", "Value": project},
+              {"Key": "stage", "Value": stage}]
+    )
 
 
 def fetch_latest_image_id(distribution: str) -> str:
