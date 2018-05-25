d84bbd789ba2908eba1ac51936c33de3cd866c30
mazerty <mazerty@gmail.com>
2018-05-25 13:15:24 +0200

    refactor: extracting subnet creation into its own function

--- src/ec2.py
+++ src/ec2.py
@@ -6,7 +6,8 @@ client = boto3.client("ec2")
 
 
 def init_environment(project: str, stage: str):
-    create_vpc_if_needed(project, stage)
+    vpc_id = create_vpc_if_needed(project, stage)
+    create_subnet_if_needed(project, stage, vpc_id)
     create_instance_if_needed(project, stage)
 
 
@@ -24,13 +25,27 @@ def create_vpc_if_needed(project: str, stage: str) -> str:
         vpc_id = create_vpc.get("Vpc").get("VpcId")
         client.get_waiter('vpc_exists').wait(VpcIds=[vpc_id])
         create_tags(project, stage, vpc_id)
+        return vpc_id
+    else:
+        return describe_vpcs.get("Vpcs")[0].get("VpcId")
 
-        print("creating subnet")
+
+def create_subnet_if_needed(project: str, stage: str, vpc_id: str) -> str:
+    print("checking subnet")
+    describe_subnets = client.describe_subnets(
+        Filters=[{"Name": "tag:project", "Values": [project]},
+                 {"Name": "tag:stage", "Values": [stage]}]
+    )
+
+    if not describe_subnets.get("Subnets"):
+        print("subnet not found, creating subnet")
+        network_cidr = config.fetch_network_cidr(project, stage)
         create_subnet = client.create_subnet(CidrBlock=network_cidr, VpcId=vpc_id)
         subnet_id = create_subnet.get("Subnet").get("SubnetId")
         create_tags(project, stage, subnet_id)
-
         return subnet_id
+    else:
+        return describe_subnets.get("Subnets")[0].get("SubnetId")
 
 
 def create_instance_if_needed(project: str, stage: str) -> str:
