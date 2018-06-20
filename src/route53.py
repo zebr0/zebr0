7da5991a5a63ded47fc9558717eeadb56078ca4f
mazerty <mazerty@gmail.com>
2018-06-20 17:08:42 +0200

    destroying dns entry with a little hack...

--- src/route53.py
+++ src/route53.py
@@ -6,6 +6,15 @@ client = boto3.client("route53")
 
 
 def create_dns_entry_if_needed(project, stage, address):
+    _change_dns_entry_if_needed("UPSERT", project, stage, address)
+
+
+def destroy_dns_entry_if_needed(project, stage):
+    _change_dns_entry_if_needed("UPSERT", project, stage, "192.0.2.1")
+    _change_dns_entry_if_needed("DELETE", project, stage, "192.0.2.1")
+
+
+def _change_dns_entry_if_needed(action, project, stage, address):
     domain_name = config.fetch_domain_name()
     if domain_name:
         hosted_zones = client.list_hosted_zones_by_name(DNSName=domain_name).get("HostedZones")
@@ -13,7 +22,7 @@ def create_dns_entry_if_needed(project, stage, address):
             client.change_resource_record_sets(
                 HostedZoneId=hosted_zones[0].get("Id"),
                 ChangeBatch={"Changes": [{
-                    "Action": "UPSERT",
+                    "Action": action,
                     "ResourceRecordSet": {
                         "Name": ".".join([stage, project, domain_name]),  # TODO
                         "Type": "A",
